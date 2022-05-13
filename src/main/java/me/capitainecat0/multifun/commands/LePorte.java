716b19fb467a08e1e1e0e6d5dd5cdc62b51ac840
CapitaineCat0 <33187305+CapitaineCat0@users.noreply.github.com>
2022-05-13 23:56:14 +0200

    Add files via upload

--- src/main/java/me/capitainecat0/multifun/commands/LePorte.java
+++ src/main/java/me/capitainecat0/multifun/commands/LePorte.java
@@ -20,31 +20,31 @@ public class LePorte implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!sender.hasPermission("multifun.leporte") || !sender.hasPermission("multifun.*")) {
-            sender.sendMessage(MultiFun.colored("&cVous n'avez pas la permission d'utiliser la commande &e/LePorte&c!"));
-            return true;
+            if (!sender.hasPermission("multifun.leporte") || !sender.hasPermission("multifun.*")) {
+                sender.sendMessage(MultiFun.colored("&cVous n'avez pas la permission d'utiliser la commande &e/LePorte&c!"));
+                return true;
 
             }else{
-            if (sender instanceof ConsoleCommandSender){
-                sender.sendMessage(MultiFun.colored("&cLa commande &e/LePorte &cest réservée aux joueurs!"));
-                return true;
-            }else{
-                ItemStack item =  new ItemStack(Material.OAK_DOOR);
-                ItemMeta meta = item.getItemMeta();
-                item.setAmount(1);
-                assert meta != null;
-                meta.setDisplayName("§e§lLePorte");
-                meta.setLore(Collections.singletonList("(La french touch de ce plugin ;])"));
-                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier("generic.attack.damage", 666.0D, AttributeModifier.Operation.ADD_NUMBER));
-                meta.addAttributeModifier(Attribute.GENERIC_MOVEMENT_SPEED, new AttributeModifier("generic.movement.speed", 0.5D, AttributeModifier.Operation.ADD_NUMBER));
-                meta.addEnchant(Enchantment.LUCK, 1, false);
-                meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-                meta.addItemFlags(ItemFlag.HIDE_PLACED_ON);
-                meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-                item.setItemMeta(meta);
-                ((Player)sender).getInventory().addItem(item);
+                if (sender instanceof ConsoleCommandSender){
+                    sender.sendMessage(MultiFun.colored("&cLa commande &e/LePorte &cest réservée aux joueurs!"));
+                    return true;
+                }else{
+                    ItemStack item =  new ItemStack(Material.OAK_DOOR);
+                    ItemMeta meta = item.getItemMeta();
+                    item.setAmount(1);
+                    assert meta != null;
+                    meta.setDisplayName("§e§lLePorte");
+                    meta.setLore(Collections.singletonList("(La french touch de ce plugin ;])"));
+                    meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier("generic.attack.damage", 666.0D, AttributeModifier.Operation.ADD_NUMBER));
+                    meta.addAttributeModifier(Attribute.GENERIC_MOVEMENT_SPEED, new AttributeModifier("generic.movement.speed", 0.5D, AttributeModifier.Operation.ADD_NUMBER));
+                    meta.addEnchant(Enchantment.LUCK, 1, false);
+                    meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+                    meta.addItemFlags(ItemFlag.HIDE_PLACED_ON);
+                    meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                    item.setItemMeta(meta);
+                    ((Player)sender).getInventory().addItem(item);
+                }
             }
-        }
         return false;
     }
 }
--- src/main/java/me/capitainecat0/multifun/events/Chat.java
+++ src/main/java/me/capitainecat0/multifun/events/Chat.java
@@ -1,6 +1,34 @@
 package me.capitainecat0.multifun.events;
 
+import me.capitainecat0.multifun.MultiFun;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class Chat implements Listener {
+    @EventHandler
+    public void OnChat(AsyncPlayerChatEvent event){
+
+            String msg = event.getMessage();
+            String prefix = "§b_Crispy_ §7>§f ";
+            Player sender = event.getPlayer();
+            if(msg.contains("tg") || msg.contains("TG")){
+                sender.sendMessage(prefix+"v");
+            }else if(msg.contains("ah") || msg.contains("Ah") || msg.contains("AH")){
+                sender.sendMessage(prefix+"Beh!");
+            }else if(msg.contains("mais")){
+                sender.sendMessage(prefix+"zon");
+            }else if(msg.contains("nan")){
+                sender.sendMessage(prefix+"te");
+            }else if(msg.contains("oui") || msg.contains("ui")){
+                sender.sendMessage(prefix+"stiti");
+            }else if(msg.contains("non")){
+                sender.sendMessage(prefix+"bril");
+            }else if(msg.contains("bruh")){
+                sender.sendMessage(prefix+"ton");
+            }else if(msg.contains("")){
+
+            }
+    }
 }
